932f174a816cb652d2e83d7a0bec2f4d241734f2
Bruno Ananias <brunomananias2009@hotmail.com>
2023-01-06 14:46:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using ListasExercicios;
+using System.Globalization;
+
+Console.WriteLine("How many employees will be registred ? ");
+int n = int.Parse(Console.ReadLine());
+
+List<Employee> list = new List<Employee>();
+
+for (int i = 1; i <= n; i++)
+{
+  
+    Console.WriteLine("Employee #" + i + ":");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salario: ");
+    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    list.Add(new Employee(id, name, salary));
+    Console.WriteLine();    
+}
+
+Console.WriteLine("Enter the employee id that will have salary increase: ");
+int searchId = int.Parse(Console.ReadLine());
+
+Employee emp = list.Find(x => x.Id == searchId);    
+if(emp != null)
+{
+    Console.WriteLine("Enter the percentage: ");
+    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    emp.IncreaseSalary(percentage);
+
+}
+else
+{
+    Console.WriteLine("This id does not exist!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees: ");
+foreach(Employee obj in list)
+{
+    Console.WriteLine(obj);
+}
+
